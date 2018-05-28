fbc28916e04d0f7789ad30feff5f08990eb1447c
sushmitha madireddy <sushmithamadireddy95@gmail.com>
2018-05-28 15:35:52 -0400

    :wq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import{BrowserModule} from '@angular/platform-browser'
 import {AppComponent} from './app.component';
 
 @NgModule({
-    imports:[BrowserModule]
-    ,
+    imports:[BrowserModule],
     declarations:[AppComponent],
     bootstrap:[AppComponent]
 
